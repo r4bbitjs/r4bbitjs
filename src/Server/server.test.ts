c8eebbd566d061d7a86bf47c00b6aad672e0d13f
Baris Balli <barisbll.dev@gmail.com>
2023-04-30 21:27:18 +0200

    unit-tests: some of the server tests implemented

--- src/Server/server.test.ts
+++ src/Server/server.test.ts
@@ -1,49 +1,121 @@
 const mockAddSetup = jest.fn();
-const mockInitRabbit = jest.fn().mockReturnValueOnce({
+const consumeMock = jest.fn();
+const channelWrapper = {
   addSetup: mockAddSetup,
-});
+  consume: consumeMock,
+  publish: jest.fn(),
+};
+const assertMock = jest.fn();
 
-jest.mock('../Init/init', () => {
-  return {
-    initRabbit: mockInitRabbit,
-  };
-});
+jest.mock('../Common/cache', () => ({
+  ConnectionSet: {
+    assert: assertMock,
+  },
+}));
+const initRabbitMock = jest.fn().mockReturnValueOnce(channelWrapper);
+jest.mock('../Init/init', () => ({
+  initRabbit: initRabbitMock,
+}));
 
 import { getServer } from './server';
-import { ServerOptions } from '../Server/server.type';
+import { AckHandler, ServerOptions, Handler } from './server.type';
 
 describe('Server tests', () => {
+  const connectionUrl = 'fake-connection-url';
+  const queueName = 'test-queue';
+  const exchangeName = 'test-exchange';
+  const routingKey = 'test-routing-key';
+
+  const serverFactory = async (
+    options: ServerOptions,
+    handlerFunc: Handler | AckHandler
+  ) => {
+    const server = await getServer(connectionUrl);
+    await server.registerRoute(
+      { queueName, routingKey, exchangeName },
+      handlerFunc,
+      options
+    );
+  };
+
   it('should call init during server instantiation', async () => {
     // given
-    const connectionUrl = 'fake-connection-url';
+    const options = {
+      consumeOptions: {
+        noAck: true,
+      },
+    };
+    serverFactory(options, jest.fn());
 
-    // when
-    await getServer(connectionUrl);
-
-    // then
-    expect(mockInitRabbit).toHaveBeenCalled();
+    // when & then
+    expect(initRabbitMock).toHaveBeenCalled();
   });
 
   it('should add setup while regustering route', async () => {
     // given
-    const connectionUrl = 'fake-connection-url';
-    const queueName = 'test-queue';
-    const exchangeName = 'test-exchange';
-    const routingKey = 'test-routing-key';
     const options: ServerOptions = {
-      noAck: true,
+      consumeOptions: {
+        noAck: true,
+      },
     };
     const handlerFunction = jest.fn();
-
     // when
-    const server = await getServer(connectionUrl);
-    await server.registerRoute(
-      { queueName, routingKey, exchangeName },
-      handlerFunction,
-      options
-    );
+    await serverFactory(options, handlerFunction);
 
     // then
-    expect(mockAddSetup).toHaveBeenCalledTimes(1);
+    expect(assertMock).toBeCalledWith(
+      channelWrapper,
+      exchangeName,
+      queueName,
+      routingKey
+    );
   });
+
+  // message should run the acknowledge function
+  it('should trigger handlerFunction on new message', async () => {
+    // given
+    const options: ServerOptions = {
+      consumeOptions: {
+        noAck: false,
+      },
+    };
+
+    const handlerSpy = jest.fn();
+    const handlerFunc: AckHandler =
+      ({ ack }) =>
+      (msg: string | object) => {
+        console.log('HANDLING...');
+        if (!msg) return;
+        if (!ack) return;
+
+        try {
+          handlerSpy();
+          ack();
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    consumeMock.mockReset();
+    consumeMock.mockImplementation((queueName, handler) => {
+      handler({
+        content: 'test',
+        properties: { correlationId: 'test', headers: {} },
+      });
+    });
+
+    // when
+    await serverFactory(options, handlerFunc);
+
+    // then
+    expect(handlerSpy).toHaveBeenCalled();
+  });
+
+  // it('should throw an error if the message is null', () => {});
+
+  // if message null
+  // if in options ack => ack !== undef (with acknowledgment)
+  // if in options nack => ack === undef (no acknowledgment)
+  // if in options responseContains => prepareResponse
+  // if preparedResponse pass to the handlerFunc
 });
