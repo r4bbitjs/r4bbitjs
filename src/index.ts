14dd176446d73f6266279863cb462c21c5284376
Baris Balli <barisbll.dev@gmail.com>
2023-09-12 20:38:29 +0300

    Exported types of Client and Server

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import { getClient } from './Client/client';
 import { setupR4bbit } from './Common/setupRabbit/setupRabbit';
 import * as ServerTypes from './Server/server.type';
 import * as ClientTypes from './Client/client.type';
+import { Client } from './Client/client';
+import { Server } from './Server/server';
 
-export { getServer, getClient, setupR4bbit, ServerTypes, ClientTypes };
+export {
+  getServer,
+  getClient,
+  setupR4bbit,
+  ServerTypes,
+  ClientTypes,
+  Client,
+  Server,
+};
